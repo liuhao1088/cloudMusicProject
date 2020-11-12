24c3d38dce88b68f36dc1d74a65e8e52be4b59e4
liuhao1088 <1350569467@qq.com>
2020-11-12 15:59:31 +0800

    “logs”

---
+++ pages/inedx/inedx.js
@@ -0,0 +1,76 @@
+// pages/inedx/inedx.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrl: [
+      'https://img10.360buyimg.com/ddimg/jfs/t1/124856/8/12620/935208/5f6097f6Efbe75dc7/68a28f980178cf14.jpg',
+      'https://s1.ax1x.com/2020/09/15/w6ExmR.md.jpg',
+      'https://img10.360buyimg.com/ddimg/jfs/t1/115298/12/19148/1094071/5f6daab9Edd0f4f92/6d281f09dc0f699e.jpg',
+      'https://s1.ax1x.com/2020/09/15/w6V09U.md.jpg'
+    ],
+    active: 0,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
