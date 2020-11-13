d54e470db39d3ffea6c6cb4df5f7be5ec1eadef1
liuhao1088 <1350569467@qq.com>
2020-11-13 10:27:58 +0800

    更新

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,13 +1,40 @@
-// pages/personal/personal.js
+let startY = 0;//手指起始的坐标
+let moveY = 0;//手指移动的坐标
+let moveDistance = 0;//手指移动的距离
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    coverTransform:'0rpx',
+    coveTransition:''
   },
-
+  handleTouchStart(even){
+    this.setData({
+      coveTransition:""
+    })
+    startY = even.touches[0].clientY;//获取手指起始的坐标
+  },
+  handleTouchMove(even){
+    moveY = even.touches[0].clientY;//获取手指移动的坐标
+    moveDistance = moveY - startY;
+    if(moveDistance<0){
+      return ;
+    }else if(moveDistance >= 80){
+      moveDistance = 80;
+    }
+    this.setData({
+      coverTransform: moveDistance+"rpx"
+    })
+    console.log(this.data.coverTransform)
+  },
+  handleTouchEnd(even){
+    this.setData({
+      coverTransform: "0rpx",
+      coveTransition:"transform 1s linear"
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 //配置服务器相关信息
 export default {
-  host: "http://192.168.0.105:3000",
+  host: "http://192.168.100.168:3000",
   moelHots:"https://liuhao1088.utools.club"
 }
 
