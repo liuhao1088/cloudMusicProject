e1b234a4ec3fcf0500beadaa0db35b86dfeeaab3
liuhao1088 <1350569467@qq.com>
2020-11-16 17:42:16 +0800

    更新

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,4 +1,5 @@
 var commom = require('../../utils/request')
+let isSend = false; //函数节流
 Page({
 
   /**
@@ -7,6 +8,8 @@ Page({
   data: {
     placeholderName: '', //默认搜索关键字
     hotList: [], //热搜榜数据
+    searchContent: '', //用户输入表单下的数据
+    searchList: [], //搜索关键字内容
   },
 
   //获取默认搜索关键字
@@ -23,6 +26,38 @@ Page({
       hotList: hotListData.data
     })
 
+  },
+  async getSearchList() {
+    if(!this.data.searchContent){
+      this.setData({
+        searchList:[]
+      })
+      return ;
+    }
+    //获取关键字内容
+    let searchListData = await commom.request('/search', {
+      keywords: this.data.searchContent,
+      limit: 10
+    }, 'GET');
+    this.setData({
+      searchList: searchListData.result.songs
+    })
+  },
+  //获取input输入的内容
+  handleInputChange(event) {
+    console.log(event.detail.value)
+    this.setData({
+      searchContent: event.detail.value.trim()
+    })
+    //函数节流
+    if (isSend) {
+      return;
+    }
+    isSend = true;
+    this.getSearchList();
+    setTimeout(() => {
+      isSend = false;
+    }, 300)
   },
 
   /**
