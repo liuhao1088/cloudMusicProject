c74efbf52891be91a8b58a14acc42515a8226f51
liuhao1088 <1350569467@qq.com>
2020-11-17 16:36:30 +0800

    更新

---
+++ pages/other/other.js
@@ -0,0 +1,72 @@
+// pages/other/other.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    item: {
+      userName: '张三',
+      age: 20
+    },
+    img01: {
+      url:'https://img11.360buyimg.com/ddimg/jfs/t1/127014/38/18295/231100/5fabaa8fE5194b55c/7e3431e9ba0a69e7.png',
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
